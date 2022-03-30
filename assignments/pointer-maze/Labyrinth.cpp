01398637a5b5bcf24a2927e8e97498412f3a3901
RandyLu0 <lurandy8@gmail.com>
2022-03-29 22:24:13 -0400

    finished

--- assignments/pointer-maze/Labyrinth.cpp
+++ assignments/pointer-maze/Labyrinth.cpp
@@ -1,18 +1,19 @@
 #include "Labyrinth.h"
-#include <set>
+#include <unordered_set>
 
-using std::set;
+using std::unordered_set;
 
 bool isPathToFreedom(MazeCell* start, const std::string& moves) {
-    set<Item> has;
+    unordered_set<Item> has;
     for(auto move : moves){
-        if(start->whatsHere != Item::NOTHING and !has.count(start->whatsHere)) has.insert(start->whatsHere);
+        if(start->whatsHere != Item::NOTHING) has.insert(start->whatsHere);
         if(move == 'N' and start->north) start = start->north;
         else if(move == 'E' and start->east) start = start->east;
         else if(move == 'S' and start->south) start = start->south;
         else if(move == 'W' and start->west) start = start->west;
         else return false;
     }
+    if(start->whatsHere != Item::NOTHING) has.insert(start->whatsHere);
     if(has.size() == 3) return true;
     return false;
 }
