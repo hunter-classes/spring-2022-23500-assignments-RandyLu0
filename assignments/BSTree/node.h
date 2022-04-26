728408ffd34fee4e77ab9a3efa68cefd364c3e91
RandyLu0 <lurandy8@gmail.com>
2022-04-26 17:09:11 -0400

    bstree

---
+++ assignments/BSTree/node.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef NODE_H
+#define NODE_H
+
+class Node{
+    private:
+        int data;
+        Node* left;
+        Node* right;
+    public:
+        Node();
+        Node(int d);
+}
+
+#endif
